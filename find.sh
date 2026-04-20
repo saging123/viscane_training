665b0ad775dd65f85e93d5ebab819342ae07d289
lynxfx <liibarra.nis@nwtf.org.ph>
2026-04-20 17:43:44 +0800

    req

--- find.sh
+++ find.sh
@@ -24,7 +24,7 @@ for Z in $ZONES; do
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
     --accelerator="count=1,type=$GPU_TYPE" \
     --tags=https-server \
-    --create-disk="auto-delete=yes,boot=yes,device-name=$NAME,image=$IMAGE,mode=rw,size=50,type=pd-balanced" \
+    --create-disk="auto-delete=yes,boot=yes,device-name=$NAME,image=$IMAGE,mode=rw,size=100,type=pd-balanced" \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
